565527c99928ddb68c458dea0092eed1d3c87eef
thlamz <thlamenza@hotmail.com>
2023-04-03 23:54:00 -0300

    Improving serialization tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,14 +1,18 @@
+import itertools
 import unittest
 
-from environment import tuple_to_base_id, base_id_to_tuple
+from base_serializer import tuple_to_base_id, base_id_to_tuple
 
 
 class MyTestCase(unittest.TestCase):
     def test_base_transformation(self):
-        data = (0, 5, 3, 1)
-        base = tuple_to_base_id(data, 6)
-
-        self.assertEqual(data, base_id_to_tuple(base, 6, 4))
+        last_base = -1
+        for test in itertools.product((i for i in range(3)),  repeat=3):
+            test = test[::-1]
+            base = tuple_to_base_id(test, 3)
+            self.assertEqual(base, last_base + 1)
+            last_base = base
+            self.assertEqual(test, base_id_to_tuple(base, 3, 3))
 
 
 if __name__ == '__main__':
