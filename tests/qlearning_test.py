e340999522b680b8c099c309239d2a4ea3a84ed0
thlamz <thlamenza@hotmail.com>
2023-04-15 15:35:27 -0300

    Better tests

---
+++ tests/qlearning_test.py
@@ -0,0 +1,43 @@
+import unittest
+from pathlib import Path
+
+from QLearning import SparseQTable
+from control import Control, MobilityCommand
+from environment import Environment
+from main import get_default_configuration
+from state_test import MobilityState
+
+
+class QTableTestCase(unittest.TestCase):
+    def test_sparse_qtable(self):
+        config = get_default_configuration()
+        q_table_file = Path("./__sparse_qtable.json")
+        config['qtable_file'] = q_table_file
+        env = Environment(config)
+
+        q_table = SparseQTable(config, env)
+        state = MobilityState([0])
+        control = Control((MobilityCommand.FORWARDS,))
+        q_table.set_q_value(state, control, 0)
+        self.assertEqual(q_table.get_q_value(state, control), 0)
+        control2 = Control((MobilityCommand.REVERSE,))
+        q_table.set_q_value(state, control2, 1)
+        self.assertEqual(q_table.get_optimal_control(state), control2)
+
+        state2 = MobilityState([1])
+        # Getting non existanting q_value returns initialization value
+        self.assertEqual(q_table.get_q_value(state2, control), config['qtable_initialization_value'])
+        # Optimal control for non existanting state is random
+        self.assertEqual(q_table.get_q_value(state2, q_table.get_optimal_control(state2)),
+                         config['qtable_initialization_value'])
+
+        q_table.export_qtable()
+
+        loaded_q_table = SparseQTable(config, env)
+        self.assertEqual(q_table.q_table, loaded_q_table.q_table)
+
+        q_table_file.unlink()
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/state_test.py
+++ tests/state_test.py
@@ -1,19 +1,7 @@
-import itertools
+
 import unittest
 
-from base_serializer import tuple_to_base_id, base_id_to_tuple
 from state import MobilityState, SignedMobilityState, CommunicationMobilityState
-
-
-class UtilityTestCase(unittest.TestCase):
-    def test_base_transformation(self):
-        last_base = -1
-        for test in itertools.product((i for i in range(3)), repeat=3):
-            test = test[::-1]
-            base = tuple_to_base_id(test, 3)
-            self.assertEqual(base, last_base + 1)
-            last_base = base
-            self.assertEqual(test, base_id_to_tuple(base, 3, 3))
 
 
 class StateSerializationTestCase(unittest.TestCase):
