3493e3dfbc91f9df79efad9154472bbd9affbac7
thlamz <thlamenza@hotmail.com>
2023-05-26 20:05:50 -0300

    Focusing on the exploration of model architectures

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         'gamma': [0.99],
         'memory_size': [10_000],
         'batch_size': [64],
-        'hidden_layer_size': [64, 256, 1024],
+        'hidden_layer_size': [64, 128, 256],
         'num_hidden_layers': [2],
         'target_network_update_rate': ['auto'],
         'optimizing_rate': [10]
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     asyncio.run(campaign_manager.run_campaign(
         {
             'num_agents': 1,
-            'mission_size': [45, 50, 60],
+            'mission_size': [60],
             'sensor_generation_probability': 0.1,
             'sensor_packet_lifecycle': math.inf,
             'controller': DQNLearner,
@@ -57,7 +57,7 @@ if __name__ == '__main__':
             # },
             'controller_config': controller_config_permutations,
             'state': CommunicationMobilityPacketsState,
-            'repetitions': [1, 2]
+            'repetitions': [1, 2, 3]
         },
         ['repetitions', 'mission_size', 'controller_config'],
         {
