c0604e137ea6c51b4651af6d92faad6f839723d2
thlamz <thlamenza@hotmail.com>
2023-07-14 21:00:47 -0300

    Rerunning QLearning results

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import argparse
 import asyncio
-import itertools
 import math
 import multiprocessing
 from pathlib import Path
 
 from DQNLearner import DQNLearner
+from QLearning import SparseQTable, QLearning
 from campaign import CampaignManager
 from rewards import smooth_unique_packets, unique_packets
 from state import CommunicationMobilityPacketsState
@@ -19,40 +19,72 @@ if __name__ == '__main__':
 
     campaign_manager = CampaignManager(Path("analysis/results.json"), max_processes=args.max_processes)
 
-    controller_config_permutation_dict = {
-        'reward_function': [smooth_unique_packets],
-        'epsilon_start': [1],
-        'epsilon_end': [0.1],
-        'learning_rate': [0.0005],
-        'gamma': [0.99],
-        'memory_size': [10_000],
-        'batch_size': [128],
-        'hidden_layer_size': [128],
-        'num_hidden_layers': [2],
-        'target_network_update_rate': [10_000],
-        'optimizing_rate': [10]
-    }
-    keys, values = zip(*controller_config_permutation_dict.items())
-    controller_config_permutations = [dict(zip(keys, v)) for v in itertools.product(*values)]
+    # controller_config_permutation_dict = {
+    #     'reward_function': [unique_packets],
+    #     'epsilon_start': [1],
+    #     'epsilon_end': [0.1],
+    #     'learning_rate': [0.0005],
+    #     'gamma': [0.99],
+    #     'memory_size': [10_000],
+    #     'batch_size': [128],
+    #     'hidden_layer_size': [128],
+    #     'num_hidden_layers': [2],
+    #     'target_network_update_rate': [10_000],
+    #     'optimizing_rate': [10]
+    # }
+    # keys, values = zip(*controller_config_permutation_dict.items())
+    # controller_config_permutations = [dict(zip(keys, v)) for v in itertools.product(*values)]
+    #
+    # asyncio.run(campaign_manager.run_campaign(
+    #     {
+    #         'num_agents': [1],
+    #         'mission_size': [10],
+    #         'sensor_generation_probability': 0.1,
+    #         'sensor_packet_lifecycle': math.inf,
+    #         'controller': DQNLearner,
+    #         'controller_config': controller_config_permutations,
+    #         'state': CommunicationMobilityPacketsState,
+    #         'repetitions': [1],
+    #         'plots': True
+    #     },
+    #     ['repetitions', 'mission_size', 'num_agents', 'controller_config'],
+    #     {
+    #         'training_steps': 1_000_000,
+    #         'testing_steps': 10_000,
+    #         'live_testing_frequency': 1_000_000,
+    #         'testing_repetitions': 1
+    #     }
+    # ))
 
-    asyncio.run(campaign_manager.run_campaign(
-        {
-            'num_agents': [1, 2, 4],
-            'mission_size': [10, 20, 40],
-            'sensor_generation_probability': 0.1,
-            'sensor_packet_lifecycle': math.inf,
-            'controller': DQNLearner,
-            'controller_config': controller_config_permutations,
-            'state': CommunicationMobilityPacketsState,
-            'repetitions': [1, 2, 3]
-        },
-        ['repetitions', 'mission_size', 'num_agents', 'controller_config'],
-        {
-            'training_steps': 10_000_000,
-            'testing_steps': 10_000,
-            'live_testing_frequency': 1_000_000,
-            'testing_repetitions': 5
-        }
-    ))
+    for num_agents in [1, 2, 4]:
+        for mission_size in [10, 20, 40]:
+            for repetition in [1, 2, 3]:
+                asyncio.run(campaign_manager.run_campaign(
+                    {
+                        'num_agents': [num_agents],
+                        'mission_size': [mission_size],
+                        'sensor_generation_probability': 0.1,
+                        'sensor_packet_lifecycle': math.inf,
+                        'controller': QLearning,
+                        'controller_config': {
+                            'epsilon_start': 1.,
+                            'epsilon_end': 0.001,
+                            'learning_rate': 0.9,
+                            'gamma': 0.99,
+                            'reward_function': unique_packets,
+                            'qtable_initialization_value': 0,
+                            'qtable_format': SparseQTable
+                        },
+                        'state': CommunicationMobilityPacketsState,
+                        'repetitions': [repetition],
+                    },
+                    ['repetitions', 'mission_size', 'num_agents'],
+                    {
+                        'training_steps': 10_000_000,
+                        'testing_steps': 10_000,
+                        'live_testing_frequency': 1_000_000,
+                        'testing_repetitions': 5
+                    }
+                ))
 
     campaign_manager.finalize()
